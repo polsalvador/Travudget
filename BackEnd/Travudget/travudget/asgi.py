43c0251097e9a3c8b764e7f5cafb52146775fa74
polsalvador <pol.salvadornogues@gmail.com>
2024-02-21 15:32:24 +0100

    Backend docker works

--- BackEnd/Travudget/travudget/asgi.py
+++ BackEnd/Travudget/travudget/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Travudget project.
+ASGI config for travudget project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Travudget.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travudget.settings')
 
 application = get_asgi_application()
--- BackEnd/Travudget/travudget/wsgi.py
+++ BackEnd/Travudget/travudget/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Travudget project.
+WSGI config for travudget project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Travudget.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travudget.settings')
 
 application = get_wsgi_application()
