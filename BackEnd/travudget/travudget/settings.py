0bf0d34560478fc5df1953cc0f2007d03ec6695e
polsalvador <pol.salvadornogues@gmail.com>
2024-03-17 21:28:25 +0100

    Tests inici sessio

--- BackEnd/travudget/travudget/settings.py
+++ BackEnd/travudget/travudget/settings.py
@@ -84,6 +84,10 @@ DATABASES = {
         'PASSWORD': 'mysql',
         'HOST': 'db', #db
         'PORT': '3306',
+    },
+    'test': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'test_db.sqlite3',
     }
 }
 
